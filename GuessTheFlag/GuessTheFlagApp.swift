f9c09b1b65443f264ad87f54c125d9744d27cd2f
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-04-12 14:40:22 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bogdan on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
