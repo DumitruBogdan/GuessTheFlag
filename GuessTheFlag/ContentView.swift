46b749a3256f9b9e16446d7cdfa40fed5f7e1ecf
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-04-13 09:27:24 +0300

    Adjusting the gradient

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -17,9 +17,9 @@ struct ContentView: View {
         VStack(spacing: 30) {
             ZStack{
                 RadialGradient(stops: [
-                    .init(color: .blue, location: 0.3),
-                    .init(color: .red, location: 0.3),
-                ], center: .top, startRadius: 200, endRadius: 700)
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+                ], center: .top, startRadius: 400, endRadius: 600)
                     .ignoresSafeArea()
                 VStack(spacing: 30){
                     VStack {
